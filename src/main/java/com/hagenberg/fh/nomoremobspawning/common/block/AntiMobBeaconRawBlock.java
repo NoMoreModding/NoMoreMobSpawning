829128cc35a46fa9e2a767e044789f6d9a87846b
Tobias Filler <64466469+TobiasFiller@users.noreply.github.com>
2021-05-04 16:36:03 +0200

    Block change onClick & recipe

--- src/main/java/com/hagenberg/fh/nomoremobspawning/common/block/AntiMobBeaconRawBlock.java
+++ src/main/java/com/hagenberg/fh/nomoremobspawning/common/block/AntiMobBeaconRawBlock.java
@@ -5,14 +5,19 @@ import com.hagenberg.fh.nomoremobspawning.core.init.BlockRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.audio.Sound;
+import net.minecraft.client.audio.SoundEventAccessor;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import org.lwjgl.system.CallbackI;
 
 public class AntiMobBeaconRawBlock extends Block {
@@ -24,10 +29,11 @@ public class AntiMobBeaconRawBlock extends Block {
     @Override
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if (player.inventory.getCurrentItem().getItem() == Items.WATER_BUCKET){
-          worldIn.removeBlock(pos , /*BlockRegistry.ANTI_MOB_BEACON_RAW.get()*/ false);
+          worldIn.setBlockState(pos , BlockRegistry.ANTI_MOB_BEACON.get().getDefaultState());
+          player.inventory.setInventorySlotContents(player.inventory.currentItem,new ItemStack(Items.BUCKET));
+          //worldIn.playSound(player,pos, , SoundCategory.BLOCKS,0,1);
 
-            player.inventory.setInventorySlotContents(player.inventory.currentItem,new ItemStack(Items.BUCKET));
-            return ActionResultType.CONSUME;
+          return ActionResultType.CONSUME;
         }
         return ActionResultType.PASS;
     }
